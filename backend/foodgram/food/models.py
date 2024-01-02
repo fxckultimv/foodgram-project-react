70511fca4b9fdc1a05c27a8d7e402ce979f70b1e
Mikhail Komissarov <datradexon@gmail.com>
2024-01-02 18:56:14 +0300

    Minor changes

--- backend/foodgram/food/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth import get_user_model
-from django.db import models
-from django.db.models import Sum
-from .validators import SlugValidator, min_amount
-
-User = get_user_model()
