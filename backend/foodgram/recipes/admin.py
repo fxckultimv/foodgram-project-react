c0b099344ac846924e973ca869c980fc19819dca
Mikhail Komissarov <datradexon@gmail.com>
2024-01-04 16:07:12 +0300

    Flake8 fixes

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -2,20 +2,24 @@ from django.contrib import admin
 
 from .models import Ingredient, Recipe, Cart, Tag, Favorite
 
+
 class IngredientsInRow(admin.TubularInline):
     model = Recipe.ingredients.through
+
 
 @admin.register(Favorite)
 class FavoriteAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'recipe']
     search_fields = ['user__username', 'user__email']
     empty_value_display = 
-    
+
+
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'measuring']
     search_fields = ['name']
     empty_value_display = EMPTY
+
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
@@ -38,6 +42,7 @@ class ShoppingCartAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'recipe']
     search_fields = ['user__username', 'user__email']
     empty_value_display = EMPTY
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
