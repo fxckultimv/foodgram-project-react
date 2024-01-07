204c7d42b2e2eff1e8f1d2d0ca93378434de1636
Mikhail Komissarov <datradexon@gmail.com>
2024-01-07 13:36:33 +0300

    Added apps to settings

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -24,6 +24,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'django_filters',
+    'users.apps.UsersConfig',
+    'api',
+    'recipes',
 ]
 
 MIDDLEWARE = [
