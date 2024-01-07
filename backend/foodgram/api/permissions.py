11eaae3ddfed3ea92bf403f920184dfe648a0365
Mikhail Komissarov <datradexon@gmail.com>
2024-01-07 14:03:20 +0300

    Added basic permissions

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -1,1 +1,10 @@
 from rest_framework import permissions
+
+class IsAuthorOrAdmin(permissions.BasePermission)
+    def has_permission(self, request, view):
+        return True
+    
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user
+                or request.user.is_staff)
