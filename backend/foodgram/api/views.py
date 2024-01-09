f1949be26704fa7b69d41a23f5a93cf23d9d34b5
Mikhail Komissarov <datradexon@gmail.com>
2024-01-09 18:16:01 +0300

    Added tag viewset

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,3 +1,17 @@
 from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+
+from .serializers import TagSerializer
+from recipes.models import Tag
+
+
+class TagViewSet(viewset.ReadOnlyModelViewSet):
+
+    queryset = Tag.objects.all()
+    permission_classes = [AllowAny, ]
+    pagination_class = None
+    setializer_class = TagSerializer
+
+
+class IngredientViewSet(viewset.ReadOnlyModelViewSet):
