373d18e95ae3837bacc45b8340febd12551ab2d3
Mikhail Komissarov <datradexon@gmail.com>
2024-01-09 18:22:16 +0300

    Added ingredients viewset

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,9 +1,11 @@
 from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-
-from .serializers import TagSerializer
-from recipes.models import Tag
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import AllowAny
+from .filters import IngredientsFilter
+from .serializers import TagSerializer, IngredientSerializer
+from recipes.models import Tag, Ingredient
 
 
 class TagViewSet(viewset.ReadOnlyModelViewSet):
@@ -15,3 +17,9 @@ class TagViewSet(viewset.ReadOnlyModelViewSet):
 
 
 class IngredientViewSet(viewset.ReadOnlyModelViewSet):
+    queryset = Ingredient.objects.all()
+    permission_classes = [AllowAny, ]
+    filter_backends = [IngredientsFilter, ]
+    serializer_class = IngredientsFilter
+    pagination_class = None
+    search_filter = ['^name', ]
