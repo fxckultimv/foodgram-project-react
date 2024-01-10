81b6a55521a032a93b3cd2bf5638a344efcb8d37
Mikhail Komissarov <datradexon@gmail.com>
2024-01-10 14:55:43 +0300

    Urls

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,3 +1,44 @@
 from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+
+from .views import (TagViewSet, IngredientViewSet, RecipeViewSet,
+                    CartViewSet, ShowSubscriptionsViewSet, FavoriteView,
+                    SubscribeView, TagViewSet, download_shopping_cart)
 
 router = DefaultRouter()
+
+router.register('tags', TagViewSet, basename='tags')
+router.register('ingredients', IngredientViewSet, basename='ingredients')
+router.register('recipes', RecipeViewSet, basename='recipes')
+
+urlpatterns = [
+    path(
+        'recipes/download_shopping_cart/',
+        download_shopping_cart,
+        name='download_shopping_cart'
+    ),
+    path(
+        'recipes/<int:id>/favorite/',
+        FavoriteView.as_view(),
+        name='favorite'
+    ),
+    path(
+        'recipes/<int:id>/shopping_cart/',
+        CartViewSet.as_view(),
+        name='shopping_cart'
+    ),
+    path(
+        'users/subscriptions/',
+        ShowSubscriptionsViewSet.as_view(),
+        name='subscriptions'
+    ),
+    path(
+        'users/<int:id>/subscribe/',
+        SubscribeView.as_view(),
+        name='subscribe'
+    ),
+    
+    path('auth/', include('djoser.urls.authtoken')),
+    path('', include('djoser.urls')),
+    path('', include(router.urls)),
+]
